3a76f4464bc63c2fa0dfa4215844c6dd0f72e4e8
GemimaOndele <128593071+GemimaOndele@users.noreply.github.com>
2023-05-26 18:18:22 +0200

    Update profil_user.php

--- profil_user.php
+++ profil_user.php
@@ -1,152 +1,203 @@
 <?php
-// Configuration de la base de données
-$servername = "localhost";
-$username = "votre_nom_utilisateur";
-$password = "votre_mot_de_passe";
-$dbname = "projet_tech_musee.sql";
+session_start();
 
-// Connexion à la base de données
-//$conn = new mysqli($servername, $username, $password, $dbname);
-//if ($conn->connect_error) {
- //   die("Échec de la connexion à la base de données : " . $conn->connect_error);
-//}
+require_once('function.php');
+$bdd = connect();
 
+// Vérification de l'authentification de l'utilisateur
+if (!isset($_SESSION['user'])) {
+    header('Location: login.php');
+    exit();
+}
 
-$errors = array();
-$success = false;
+$user = $_SESSION['user'];
 
+// Traitement du formulaire de modification des coordonnées
+if (isset($_POST['update'])) {
+    // Récupération des données du formulaire
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $password = $_POST['password'];
+    $sexe = $_POST['sexe'];
+    $adresse = $_POST['adresse'];
+    $pays = $_POST['pays'];
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Validation des données
-    $nom = validateInput($_POST["nom"]);
-    $prenom = validateInput($_POST["prenom"]);
-    $sexe = validateInput($_POST["sexe"]);
-    $pays = validateInput($_POST["pays"]);
-    $email = validateInput($_POST["email"]);
+    $errors = array();
 
-    // Validation de l'avatar
-    $avatar = $_FILES["avatar"];
-    $avatarName = $avatar["name"];
-    $avatarTmpName = $avatar["tmp_name"];
-    $avatarSize = $avatar["size"];
-    $avatarError = $avatar["error"];
-
-    // Vérification des champs obligatoires
     if (empty($nom)) {
-        $errors[] = "Le champ Nom est obligatoire.";
+        $errors[] = "Le nom est obligatoire.";
     }
 
     if (empty($prenom)) {
-        $errors[] = "Le champ Prénom est obligatoire.";
+        $errors[] = "Le prénom est obligatoire.";
     }
 
-    if (empty($email)) {
-        $errors[] = "Le champ Adresse e-mail est obligatoire.";
-    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors[] = "L'adresse e-mail n'est pas valide.";
+    if (!empty($password)) {
+        // Si un nouveau mot de passe est saisi, on le met à jour
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+        $sql = "UPDATE users SET password = :password WHERE id = :id;";
+        $sth = $bdd->prepare($sql);
+        $sth->execute([
+            'password' => $hashedPassword,
+            'id' => $user['id']
+        ]);
     }
 
-    // Vérification des erreurs avant la sauvegarde des données
-    if (empty($errors)) {
-        // Vérification et traitement de l'avatar
-        if ($avatarError === UPLOAD_ERR_OK) {
-            $avatarPath = "avatars/" . $avatarName;
-            move_uploaded_file($avatarTmpName, $avatarPath);
-        } elseif ($avatarError !== UPLOAD_ERR_NO_FILE) {
-            $errors[] = "Une erreur est survenue lors du téléchargement de l'avatar.";
+    if (empty($sexe)) {
+        $errors[] = "Le sexe est obligatoire.";
+    }
+
+    if (empty($adresse)) {
+        $errors[] = "L'adresse est obligatoire.";
+    }
+
+    if (empty($pays)) {
+        $errors[] = "Le pays est obligatoire.";
+    }
+
+    // Gestion de l'avatar
+    if ($_FILES['avatar']['name']) {
+        $avatar = $_FILES['avatar'];
+
+        $allowedTypes = array('image/jpeg', 'image/png');
+        $maxSize = 2 * 1024 * 1024; // 2MB
+
+        // Vérification du type de fichier
+        if (!in_array($avatar['type'], $allowedTypes)) {
+            $errors[] = "Le type de fichier de l'avatar n'est pas valide. Veuillez choisir une image au format JPEG ou PNG.";
         }
 
-        // Échappement des données pour éviter les injections SQL
-        $nom = $conn->real_escape_string($nom);
-        $prenom = $conn->real_escape_string($prenom);
-        $sexe = $conn->real_escape_string($sexe);
-        $pays = $conn->real_escape_string($pays);
-        $email = $conn->real_escape_string($email);
-        $avatarPath = $conn->real_escape_string($avatarPath);
-
-        // Requête SQL pour la mise à jour des données de l'utilisateur
-        $sql = "UPDATE utilisateurs SET nom='$nom', prenom='$prenom', sexe='$sexe', pays='$pays', email='$email', avatar='$avatarPath' WHERE id=$userId";
-
-        if ($conn->query($sql) === TRUE) {
-            $success = true;
-        } else {
-            $errors[] = "Une erreur est survenue lors de la mise à jour des données : " . $conn->error;
+        // Vérification de la taille du fichier
+        if ($avatar['size'] > $maxSize) {
+            $errors[] = "La taille de l'avatar dépasse la limite autorisée (2MB). Veuillez choisir une image plus petite.";
         }
+
+        // Génération d'un nom de fichier unique pour l'avatar
+        $avatarName = uniqid() . '_' . $avatar['name'];
+
+        // Déplacement du fichier temporaire vers le dossier des avatars
+        $destination = 'avatars/' . $avatarName;
+        move_uploaded_file($avatar['tmp_name'], $destination);
+
+        // Mise à jour du nom de fichier de l'avatar dans la base de données
+        $sql = "UPDATE users SET avatar = :avatar WHERE id = :id;";
+        $sth = $bdd->prepare($sql);
+        $sth->execute([
+            'avatar' => $avatarName,
+            'id' => $user['id']
+        ]);
+    }
+
+    // Vérification des erreurs
+    if (count($errors) === 0) {
+        // Mise à jour des coordonnées dans la base de données
+        $sql = "UPDATE users SET nom = :nom, prenom = :prenom, sexe = :sexe, adresse = :adresse, pays = :pays WHERE id = :id;";
+        $sth = $bdd->prepare($sql);
+        $sth->execute([
+            'nom' => $nom,
+            'prenom' => $prenom,
+            'sexe' => $sexe,
+            'adresse' => $adresse,
+            'pays' => $pays,
+            'id' => $user['id']
+        ]);
+
+        // Redirection vers la page de profil avec un message de succès
+        header('Location: profil_user.php?success=1');
+        exit();
     }
 }
 
+// Récupération des données de l'utilisateur depuis la base de données
+$sql = "SELECT * FROM users WHERE id = :id;";
+$sth = $bdd->prepare($sql);
+$sth->execute([
+    'id' => $user['id']
+]);
 
-function validateInput($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
-
-
-$userId = 1; // ID de l'utilisateur à récupérer (à adapter selon votre système)
-$sql = "SELECT * FROM utilisateurs WHERE id=$userId";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    $row = $result->fetch_assoc();
-    $nom = $row["nom"];
-    $prenom = $row["prenom"];
-    $sexe = $row["sexe"];
-    $pays = $row["pays"];
-    $email = $row["email"];
-    $avatar = $row["avatar"];
-} else {
-    $errors[] = "Utilisateur non trouvé.";
-}
-
-
-$conn->close();
+$user = $sth->fetch();
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Profil Utilisateur</title>
+    <title>Profil utilisateur</title>
+    <style>
+        body {
+            background-image: url(img/csm_img_bandeau_musee_ec5567aba3.jpg);
+            background-size: cover;
+            background-position: center;
+        }
+    </style>
+    <link rel="stylesheet" href="Style/login.css">
 </head>
 <body>
-    <h1>Profil Utilisateur</h1>
+    <?php require_once('_header.php'); ?>
 
-    <?php if ($success): ?>
-        <div class="success">Les données ont été mises à jour avec succès.</div>
-    <?php endif; ?>
+    <div class="center">
+        <h1>Profil utilisateur</h1>
 
-    <?php if (!empty($errors)): ?>
-        <div class="errors">
-            <?php foreach ($errors as $error): ?>
-                <p><?php echo $error; ?></p>
-            <?php endforeach; ?>
-        </div>
-    <?php endif; ?>
+        <?php
+        // Affichage des erreurs
+        if (isset($errors) && count($errors) > 0) {
+            echo '<div class="errors">';
+            foreach ($errors as $error) {
+                echo '<div>' . $error . '</div>';
+            }
+            echo '</div>';
+        }
 
-    <form method="POST" enctype="multipart/form-data">
-        <label for="nom">Nom :</label>
-        <input type="text" name="nom" value="<?php echo $nom; ?>">
+        // Affichage du message de succès après la mise à jour des coordonnées
+        if (isset($_GET['success']) && $_GET['success'] === '1') {
+            echo '<div class="success">Vos coordonnées ont été mises à jour avec succès.</div>';
+        }
+        ?>
 
-        <label for="prenom">Prénom :</label>
-        <input type="text" name="prenom" value="<?php echo $prenom; ?>">
+        <form action="" method="post" enctype="multipart/form-data">
+            <div class="txt_field">
+                <input type="text" name="nom" id="nom" value="<?php echo $user['nom']; ?>" required />
+                <label for="nom">Nom</label>
+            </div>
 
-        <label for="sexe">Sexe :</label>
-        <select name="sexe">
-            <option value="homme" <?php if ($sexe === "homme") echo "selected"; ?>>Homme</option>
-            <option value="femme" <?php if ($sexe === "femme") echo "selected"; ?>>Femme</option>
-        </select>
+            <div class="txt_field">
+                <input type="text" name="prenom" id="prenom" value="<?php echo $user['prenom']; ?>" required />
+                <label for="prenom">Prénom</label>
+            </div>
 
-        <label for="pays">Pays :</label>
-        <input type="text" name="pays" value="<?php echo $pays; ?>">
+            <div class="txt_field">
+                <input type="password" name="password" id="password" />
+                <label for="password">Mot de passe</label>
+            </div>
 
-        <label for="email">Adresse e-mail :</label>
-        <input type="text" name="email" value="<?php echo $email; ?>">
+            <div class="txt_field">
+                <select name="sexe" id="sexe" required>
+                    <option value="">Sélectionnez le sexe</option>
+                    <option value="homme" <?php echo ($user['sexe'] === 'homme') ? 'selected' : ''; ?>>Homme</option>
+                    <option value="femme" <?php echo ($user['sexe'] === 'femme') ? 'selected' : ''; ?>>Femme</option>
+                </select>
+                <label for="sexe">Sexe</label>
+            </div>
 
-        <label for="avatar">Avatar :</label>
-        <input type="file" name="avatar">
+            <div class="txt_field">
+                <input type="text" name="adresse" id="adresse" value="<?php echo $user['adresse']; ?>" required />
+                <label for="adresse">Adresse postale</label>
+            </div>
 
-        <input type="submit" value="Enregistrer">
-    </form>
+            <div class="txt_field">
+                <input type="text" name="pays" id="pays" value="<?php echo $user['pays']; ?>" required />
+                <label for="pays">Pays</label>
+            </div>
+
+            <div class="txt_field">
+                <input type="file" name="avatar" id="avatar" accept="image/*" />
+                <label for="avatar">Avatar</label>
+            </div>
+
+            <div>
+                <input type="submit" class="btn btn-green" name="update" value="Enregistrer" />
+            </div>
+        </form>
+    </div>
 </body>
 </html>
