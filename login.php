4829b08319a984349b41f544e4fe3e9900817377
GemimaOndele <128593071+GemimaOndele@users.noreply.github.com>
2023-05-28 01:57:46 +0200

    login.php

--- login.php
+++ login.php
@@ -8,38 +8,32 @@ if (isset($_POST["send"])) {
 
     $sth = $bdd->prepare($sql);
 
-
     if (!$sth) {
-        die("Error during prepare: " . $bdd->errorInfo()[2]);
+        $errorInfo = $bdd->errorInfo();
+        die("Error during prepare: " . $errorInfo[2]);
     }
 
-
     $sth->execute([
-        'email'     => $_POST['email']
+        'email' => $_POST['email']
     ]);
 
     $user = $sth->fetch();
 
-    
-
     if ($user && password_verify($_POST['password'], $user['password'])) {
-        //dd($user);
-
-
         if ($_SESSION['user']['validated'] == 0) {
             $_SESSION['user'] = $user;
             header('Location: login.php');
         } else {
-
             header('Location: register.php');
         }
     } else {
-        $msg = "Email ou mot de passe incorrect !";
+        $msg = "Email or password incorrect!";
     }
 }
+
+require_once('_header.php');
 ?>
 
-<?php require_once('_header.php'); ?>
 <link rel="stylesheet" href="Style/login.css">
 <style>
     body {
@@ -48,9 +42,10 @@ if (isset($_POST["send"])) {
         background-position: center;
     }
 </style>
+
 <div class="center">
     <form action="" method="post">
-        <h1>Welcome back !</h1>
+        <h1>Welcome back!</h1>
 
         <?php if (isset($msg)) {
             echo "<div>" . $msg . "</div>";
@@ -64,21 +59,17 @@ if (isset($_POST["send"])) {
             <input type="password" name="password" id="password" required />
             <label for="password">Password</label>
         </div>
-        <div class="pass"><a class="pass" href="#"> Forgot Password ?</a></div>
+        <div class="pass"><a class="pass" href="#"> Forgot Password?</a></div>
         <div>
             <input type="submit" name="send" value="Connexion" />
         </div>
         <div class="signup_link">
-            Not a member ?<a href="register.php">Sign in</a>
+            Not a member? <a href="register.php">Sign in</a>
             <br>
-            Are you an admin ?<a href="login.php">log in</a>
+            Are you an admin? <a href="login.php">Log in</a>
         </div>
     </form>
 </div>
 
 </body>
-
 </html>
-
-
-</div>
