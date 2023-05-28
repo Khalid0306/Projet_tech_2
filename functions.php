5b0c506d8e40b19b734791f44b2c47eb57fa3675
GemimaOndele <128593071+GemimaOndele@users.noreply.github.com>
2023-05-28 02:00:21 +0200

    functions.php

--- functions.php
+++ functions.php
@@ -1,23 +1,25 @@
 <?php
 
-    session_start();
+session_start();
 
-    function dd($var) {
-        echo "<pre>";
-        var_dump($var);
-        echo "</pre>";
-        
-        die();
+function dd($var) {
+    echo "<pre>";
+    var_dump($var);
+    echo "</pre>";
+    die();
+}
+
+function connect() {
+    $host = 'localhost';
+    $dbname = 'projet_tech_musee';
+    $username = 'root';
+    $password = '';
+
+    try {
+        $bdd = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $bdd;
+    } catch (PDOException $e) {
+        die("Erreur de connexion à la base de données : " . $e->getMessage());
     }
-
-    function connect () {
-        $link = new PDO(
-            
-            'mysql:dbname=projet_tech_musee;host=localhost:3307', 
-            'root', 
-            ''
-
-        );
-
-        return $link;
-    }
+}
